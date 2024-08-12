4b5e3fa85739732bd3b7c6ef0501ee9d61cf2a64
JustTheCoolest <balu.somenumbers@gmail.com>
2024-08-13 00:29:22 +0530

    gemini_chat

---
+++ gemini_chat.py
@@ -0,0 +1,18 @@
+import google.generativeai as genai
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv() ## load all the environment variables
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+model = genai.GenerativeModel('gemini-1.5-flash')
+
+chat = model.start_chat()
+prompt = "A beautiful sunset over the city"
+response = chat.send_message(prompt)
+# print(response)
+print(response.text)
+
+# print(response.text)
